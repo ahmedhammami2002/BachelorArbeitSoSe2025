8ee97e9b47233e6ae00b6a41bdb597814bf03117
Ahmed Hammami <ahammami@techfak.uni-bielefeld.de>
2025-06-24 03:19:02 +0200

    fixed some errors did the plotting.

--- Helper_functions.py
+++ Helper_functions.py
@@ -1,30 +1,25 @@
 import torch
 
-def split_by_classification(model, X, device='cpu'):
+def split_by_classification(model, X):
     """
     Split data into positively and negatively classified samples
     """
 
     model.eval()
     with torch.no_grad():
-        X = X.to(device)
-        model = model.to(device)
 
         # Get predictions
         logits = model(X)
         predictions =  torch.argmax(logits, dim=1)
 
         # Convert boolean masks to indices
-        pos_indices = torch.where(predictions == 1)[0].cpu().numpy()
-        neg_indices = torch.where(predictions == 0)[0].cpu().numpy()
+        pos_indices = torch.where(predictions == 1)[0].numpy()
+        neg_indices = torch.where(predictions == 0)[0].numpy()
 
         # Split the data
         X_positive = X[predictions == 1]
         X_negative = X[predictions == 0]
 
-        # Move back to CPU if needed
-        X_positive = X_positive.cpu()
-        X_negative = X_negative.cpu()
 
         return X_positive, X_negative, pos_indices, neg_indices
 
